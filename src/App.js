5966ff8f09bef4d8b7697cbf354f7aa5275a627f
seungJuAn <sjan6964@naver.com>
2022-06-10 00:32:45 +0900

    리액트 state 사용 방법 및 리액트 관용 방식

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 
+import React , {useState} from 'react';
 import './App.css';
 
 function App() {
-  let mainData = '짜장면 너무 배불러'
+  let [글제목 , 글제목변경 ] = useState(['남자코트 추천', '인천대 학식', '내 꿈은 연봉1억 개발자']);
+
+
+  // 정렬 버튼 클릭시 실행되는 함수(글자 정렬)
+  function sortToString(){
+    let copyData = [...글제목]; // 리액트 관용(기존 데이터를 깊은 복사를 통해 신규 데이터 생성)
+    copyData.sort(); // 신규 데이터로 조작
+    글제목변경(copyData); // 기존 데이터를 신규 데이터와 변경
+  }
+  
   return (
     <div className="App">
-        <div className = "black-nav">
-          <div>테스트 개발</div>
+        <div className='black_nav'>
+          <div>리액트 연습</div>
         </div>
-        <div className = 'list'>
-          <h3>mainData</h3>
-          <p>하지만 맛잇어</p>
+        <button className='sortBtn' onClick={sortToString}>
+          정렬
+        </button>
+        <div className='list'>
+          <h3>{글제목[0]}</h3>
+          <hr/>
+        </div>
+        <div className='list'>
+          <h3>{글제목[1]}</h3>
+          <hr/>
+        </div>
+        <div className='list'>
+          <h3>{글제목[2]}</h3>
+          <hr/>
         </div>
     </div>
   );
