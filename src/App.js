6cac08961c5ea1a6c0802e0d5367eca9fa5fe9ad
seungJuAn <sjan6964@naver.com>
2022-06-13 11:07:40 +0900

    props, component 구조 파악

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 
 function App() {
   let [글제목 , 글제목변경 ] = useState(['남자코트 추천', '인천대 학식', '내 꿈은 연봉1억 개발자']);
-
+  let [books, booksChange] = useState({
+    name : '올해 여름은 매우 더울것같음',
+    day : '2022.06.11',
+    info : '운전연습 해야됨'
+  })
 
   // 정렬 버튼 클릭시 실행되는 함수(글자 정렬)
   function sortToString(){
@@ -12,7 +16,7 @@ function App() {
     copyData.sort(); // 신규 데이터로 조작
     글제목변경(copyData); // 기존 데이터를 신규 데이터와 변경
   }
-  
+  console.log({books})
   return (
     <div className="App">
         <div className='black_nav'>
@@ -33,8 +37,20 @@ function App() {
           <h3>{글제목[2]}</h3>
           <hr/>
         </div>
+
+        <Modal books = {books}/>
     </div>
   );
+}
+
+function Modal(books){
+  return (
+    <div className='modal'>
+      <h2>제목 : {books.books.name}</h2>
+      <p>날짜 : {books.books.day}</p>
+      <p>상세내용 : {books.books.info}</p>
+    </div>
+  )
 }
 
 export default App;
